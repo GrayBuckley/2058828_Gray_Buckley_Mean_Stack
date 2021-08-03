0f981ecb66825e3fb26458fcba6760a44295deac
Gray <u1166237@eng.utah.edu>
2021-08-03 12:43:41 -0700

    clearForm()

--- Phase 1/budget-planner/scripts.js
+++ Phase 1/budget-planner/scripts.js
@@ -1,4 +1,11 @@
 function showForm() {
+	console.log("arrived in showForm()");
 	var form = document.getElementById("form");
 	form.style.display = "block";
+}
+
+function clearForm() {
+	console.log("arrived in clearForm()");
+	var cname = document.getElementById("cname");
+	cname.value = "";
 }
