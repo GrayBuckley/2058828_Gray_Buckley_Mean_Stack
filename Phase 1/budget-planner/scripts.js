dc1f8d953f99db16abe1d79bc375243f97420a28
Gray <u1166237@eng.utah.edu>
2021-08-03 15:07:29 -0700

    teams.html, table drawing, output captured

--- Phase 1/budget-planner/scripts.js
+++ Phase 1/budget-planner/scripts.js
@@ -1,7 +1,43 @@
 function showForm() {
-	console.log("arrived in showForm()");
+	//console.log("arrived in showForm()");
 	var form = document.getElementById("form");
 	form.style.display = "block";
+}
+
+function budgetButtonClick(){
+	showTable();
+	cleanTable();
+	var total = 0;
+	var records = getRecords();
+	records.forEach(record => {
+		insertTableRow(record.cname, record.pname, record.budget);
+		total += parseInt(record.budget,10);
+	});
+	insertTableRow("Total", "", total);
+}
+
+function showTable() {
+	document.getElementById("tableDiv").style.display = "block";
+}
+
+function numTableRows() {
+	return document.getElementById("table").rows.length;
+}
+
+function cleanTable(){
+	var table = document.getElementById("table");
+	while(numTableRows() > 1) table.deleteRow(1);
+}
+
+function insertTableRow(cname, pname, budget){
+	var table = document.getElementById("table");
+	var row = table.insertRow(-1);
+	var cnameCell = row.insertCell(0);
+	var pnameCell = row.insertCell(1);
+	var budgetCell = row.insertCell(2);
+	cnameCell.innerHTML = cname;
+	pnameCell.innerHTML = pname;
+	budgetCell.innerHTML = budget;
 }
 
 function clearForm() {
