f912ed4117803b486cd69213d33fcb7077a92223
Gray <u1166237@eng.utah.edu>
2021-08-03 14:06:23 -0700

    newBudget(), addBttnOnClick finished

--- Phase 1/budget-planner/scripts.js
+++ Phase 1/budget-planner/scripts.js
@@ -18,5 +18,36 @@ function hideForm() {
 
 function addBttnOnClick() {
 	hideForm();
+	newBudget(
+		document.getElementById("cname").value,
+		document.getElementById("pname").value,
+		document.getElementById("budget").value
+	);
 	clearForm();
+}
+
+function buildStorageRecord(cname, pname, budget) {
+	var o = {
+		cname: cname,
+		pname: pname,
+		budget: budget
+	}
+	return o;
+}
+
+function getRecords() {
+	return JSON.parse(localStorage.getItem("records"));
+}
+
+function appendRecord(newRecord){
+	var records = getRecords();
+	if(records === null) records = [];
+	//console.log(records);
+	records.push(newRecord);
+	//console.log(records);
+	localStorage.setItem("records", JSON.stringify(records));
+}
+
+function newBudget(cname, pname, budget) {
+	appendRecord(buildStorageRecord(cname, pname, budget));
 }
