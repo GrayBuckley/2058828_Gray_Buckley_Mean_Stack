bd3f6c48e0b3db9759ba288e01eb14544a3b0ba5
Gray <u1166237@eng.utah.edu>
2021-08-26 14:07:33 -0700

    client dom manip

---
+++ Phase 3/planner/client/controller.ts
@@ -0,0 +1,41 @@
+
+
+function addClick(){
+    let eid = (document.getElementById("eidInput") as HTMLInputElement).value;
+    let tid = (document.getElementById("tidInput") as HTMLInputElement).value;
+    let taskDesc = (document.getElementById("tdescInput") as HTMLInputElement).value;
+    let due = (document.getElementById("deadlineInput") as HTMLInputElement).value;
+    
+}
+function deleteClick(){
+    let tid = (document.getElementById("tidDelete") as HTMLInputElement).value;
+}
+function listClick(){
+    clearTable(document.getElementById("table") as HTMLTableElement);
+}
+function getRecieved(res:string){
+    var a = JSON.parse(res) as any[];
+    a.forEach(t => {
+        var row = document.createElement("tr");
+        var eidD = document.createElement("td");
+        var tidD = document.createElement("td");
+        var descD = document.createElement("td");
+        var dueD = document.createElement("td");
+        eidD.innerText = t.empID;
+        tidD.innerText = t.taskID;
+        descD.innerText = t.task;
+        dueD.innerText = t.deadline;
+        row.appendChild(eidD);
+        row.appendChild(tidD);
+        row.appendChild(descD);
+        row.appendChild(dueD);
+        document.getElementById("table").appendChild(row);
+    });
+}
+
+function clearTable(table:HTMLTableElement){
+    var rows = table.rows;
+    var i = rows.length;
+    while (--i) 
+        rows[i].parentNode.removeChild(rows[i]);
+}
