6449974bf19e4a334ce37916d88ddc3a4d646985
Gray <u1166237@eng.utah.edu>
2021-09-07 12:17:38 -0700

    full backend, add frontend

--- Phase 3/mongo-site/server/server.js
+++ Phase 3/mongo-site/server/server.js
@@ -1,4 +1,6 @@
 const { MongoClient } = require('mongodb');
+const http = require('http');
+var url = require("url");
 const uri = "mongodb+srv://ILP:TATA@cluster0.tzyke.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 const collection = "courses";
@@ -44,3 +46,36 @@ function deleteCourse(id){
         });
     });
 }
+
+function fetchCourses(result){
+    let courses = [];
+    client.connect(err => {
+        const dbo = client.db("test");
+        dbo.createCollection(collection, (err,res) => {
+            dbo.collection(collection).find().forEach(doc => {
+                courses.push(doc);
+            }).then(() => {
+                result.write(JSON.stringify(courses));
+                result.end();
+            });
+        });
+    });
+}
+
+http.createServer((req,res) => {
+    res.writeHead(200, { 'Access-Control-Allow-Origin': '*' });
+    var urlObj = url.parse(req.url, true);
+    var queryObject = urlObj.query;
+    if(urlObj.pathname == "/fetch"){
+        fetchCourses(res);
+    }
+    else {
+        if(urlObj.pathname == "/create")
+            createCourse(queryObject.id, queryObject.name, queryObject.description, queryObject.amount);
+        if(urlObj.pathname == "/change")
+            changeCourse(queryObject.id, queryObject.amount);
+        if(urlObj.pathname == "/delete")
+            deleteCourse(queryObject.id);
+        res.end();
+    }
+}).listen(8080);
